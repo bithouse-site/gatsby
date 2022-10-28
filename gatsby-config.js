5f17c52b8705ee6d794c7c8cd846c37d34703ede
Martin <martin.marino@bitlogic.io>
2022-10-28 09:11:10 -0300

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-theme-portfolio-minimal",
+      options: {
+        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        manifestSettings: {
+          favicon: "./content/images/favicon.png", // Path is relative to the root
+          siteName: "My Minimal Portfolio", // Used in manifest.json
+          shortName: "Portfolio", // Used in manifest.json
+          startUrl: "/", // Used in manifest.json
+          backgroundColor: "#FFFFFF", // Used in manifest.json
+          themeColor: "#000000", // Used in manifest.json
+          display: "minimal-ui", // Used in manifest.json
+        },
+        contentDirectory: "./content",
+        blogSettings: {
+          path: "/blog", // Defines the slug for the blog listing page
+          usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
+        },
+        // googleAnalytics: {
+        //     trackingId: "UA-XXXXXX-X",
+        //     anonymize: true, // Default true
+        //     environments: ["production", "development"] // Default ["production"]
+        // }
+      },
+    },
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: `g3ub24w8`,
+        dataset: `develop`,
+        // a token with read permissions is required
+        // if you have a private dataset
+        //token: process.env.SANITY_TOKEN,
+
+        // If the Sanity GraphQL API was deployed using `--tag <name>`,
+        // use `graphqlTag` to specify the tag name. Defaults to `default`.
+        graphqlTag: 'default',
+      },
+    },
+    `gatsby-plugin-image`,
+  ],
+};

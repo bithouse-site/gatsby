7b8cb4feafe9096cb48dad15d5839b379327d73e
Drina <drina.rincon@bitlogic.io>
2022-11-02 11:13:10 -0300

    add gatsby theme components to create the page with our components

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,18 +10,25 @@ import {
   Seo,
 } from "gatsby-theme-portfolio-minimal";
 
+import { PageBit } from "../components/PageBit/index";
+
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Seo title='Gatsby Starter for Portfolio Minimal' />
       <Page useSplashScreenAnimation>
-        <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
-        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        <HeroSection sectionId='hero' />
+        <ArticlesSection
+          sectionId='articles'
+          heading='Latest Articles'
+          sources={["Medium"]}
+        />
+        <AboutSection sectionId='about' heading='About Portfolio Minimal' />
+        <InterestsSection sectionId='details' heading='Details' />
+        <ProjectsSection sectionId='features' heading='Built-in Features' />
+        <ContactSection sectionId='github' heading='Issues?' />
       </Page>
+      <PageBit useSplashScreenAnimation />
     </>
   );
 }
